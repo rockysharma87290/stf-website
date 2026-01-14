bb0bc4f6708045a30e8db9bcb78d5440dfcff903
rockysharma87290 <rockysharma87290@gmail.com>
2026-01-14 08:42:13 +0530

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,19 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAOkCXeMxb-Y3Y32KzWyyIEc8hmgLwzNww",
-  authDomain: "sanatan-task-force.firebaseapp.com",
-  projectId: "sanatan-task-force",
-  storageBucket: "sanatan-task-force.firebasestorage.app",
-  messagingSenderId: "578515037703",
-  appId: "1:578515037703:web:d6f3bb83a06635356104a2"
-};
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAOkCXeMxb-Y3Y32KzWyyIEc8hmgLwzNww",
+    authDomain: "sanatan-task-force.firebaseapp.com",
+    projectId: "sanatan-task-force",
+    storageBucket: "sanatan-task-force.firebasestorage.app",
+    messagingSenderId: "578515037703",
+    appId: "1:578515037703:web:d6f3bb83a06635356104a2"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
