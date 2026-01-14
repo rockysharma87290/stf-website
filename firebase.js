7e1da8344d896a8af3ab05ba5f72259638977cc6
rockysharma87290 <rockysharma87290@gmail.com>
2026-01-14 08:35:14 +0530

    firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAOkCXeMxb-Y3Y32KzWyyIEc8hmgLwzNww",
+  authDomain: "sanatan-task-force.firebaseapp.com",
+  projectId: "sanatan-task-force",
+  storageBucket: "sanatan-task-force.firebasestorage.app",
+  messagingSenderId: "578515037703",
+  appId: "1:578515037703:web:d6f3bb83a06635356104a2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
